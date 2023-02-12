3047bd4a185927d8670773f7666340be9d6a2c51
Rushikesh45Susar <rushikeshsusar45634@gmail.com>
2023-02-12 19:44:12 +0530

    topbar change

--- frontend/src/components/mini_components/Topbar.jsx
+++ frontend/src/components/mini_components/Topbar.jsx
@@ -24,7 +24,7 @@ const Topbar = (props) => {
       setProfileHeight(0);
       document.getElementById("profileOption").style.height = "0px";
     }
-
+  }
   function handleLogout() {
     localStorage.clear()
     navigate("/login")
