ba0047054263e5d7f55d6a2ca2d56b03ab46faf4
Akash <akashmete129@gmail.com>
2023-03-12 19:06:06 +0530

    choose image file, send image to bucket, get image url done

--- frontend/src/components/Setting.jsx
+++ frontend/src/components/Setting.jsx
@@ -6,12 +6,17 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import server from "./server";
 import axios from "axios";
+import AWS from 'aws-sdk';
+
 
 
 const Setting = () => {
   const account = JSON.parse(localStorage.getItem("login"));
-  const [link, setLink] = useState({ link: "" });
-
+  const [imglink, setLink] = useState([{ link: "" }]);
+  const [file, setFile] = useState([null]);
+  const [imageUrl, setImageUrl] = useState([{
+    link: ""
+}]);
   const useInputs = (initialValue) => {
     const [value, setValue] = useState(initialValue);
     const handleChange = (e) => {
@@ -32,9 +37,45 @@ const Setting = () => {
   const email = useInputs('');
   const phone = useInputs('');
 
+  //s3 bucket image upload
+  AWS.config.update({
+    accessKeyId: 'AKIAUXONWQ3HERXZEX5M',
+    secretAccessKey: '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',
+    region: 'ap-south-1',
+    signatureVersion: 'v4',
+});
+
+const handleInputChange = (e) => {
+  setFile(e.target.files);
+}
 
   const handleReset = async () => {
+    //generating and uploading image url to s3 bucket
+    const s3 = new AWS.S3();
+        const formData = new FormData();
+        const urls = [];
+
+    formData.append("image",file[0]);
+    const acnt_Img = file[0];
+    const params = {
+      Bucket: 'image-orders-bucket/acnt-img',
+                Key: `${Date.now()}.${acnt_Img.name}`,
+                Body: acnt_Img,
+                ContentEncoding: "base64",
+                ContentType: file.type,
+    };
+    const {Location} = await s3.upload(params).promise();
+    urls.push(Location);
+    var obj = { link: urls[0]};
+    setLink([...imglink,obj]);
+    alert("Image uploaded to s3 bucket.")
+    console.log('Uploading to s3',obj.link);
+    
+
+    //use this link to push to mongodb with other data
+   var link = obj.link;
     const data = await fetch(`${server}/auth/ResetLoggedUserData/${account._id}`, {
+      
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -45,6 +86,7 @@ const Setting = () => {
     const res = await data.json();
     localStorage.setItem("login", JSON.stringify(res.save));
     console.log(res)
+   
   };
 
   useEffect(() => {
@@ -70,11 +112,13 @@ const Setting = () => {
               <div>
                 <label for="formFile" class="form-label">Upload Image</label>
                 <input class="form-control" type="file"
+                multiple="false" accept="image/*"
                   name="link"
-                  value={link.link}
-                  onChange={(event) => {
-                    setLink((prev) => ({ ...prev, link: event.target.value }));
-                  }}
+                  // value={link.link}
+                  // onChange={(event) => {
+                  //   setLink((prev) => ({ ...prev, link: event.target.files }));
+                  // }}
+                  onChange={handleInputChange}
                   id="formFile" />
               </div>
 
