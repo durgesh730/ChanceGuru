44619b36203178639e249739b04a7af304804af6
Mandar86 <mandarjvategavkar45@gmail.com>
2023-03-13 22:54:21 +0530

    report table and chat report done

--- backend/db/chatModel.js
+++ backend/db/chatModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const chatModel = mongoose.Schema(
   {
-    chatName: { type: String, trim: true },
+    status: { type: String },
     unreadCount: { type: Number, default: 0 },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message"},
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,7 +4,8 @@ const {
   fetchChats,
   updateUnRead,
   incrementChat,
-  getUnReadCount
+  getUnReadCount,
+  reportChat
 } = require("../controllers/chatControllers");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -15,6 +16,7 @@ router.route("/").get(protect, fetchChats);
 router.route("/updateUnreadCount").put(protect, updateUnRead);
 router.route("/incrChat").put(protect, incrementChat);
 router.route("/getUnreadCount").get(protect, getUnReadCount);
+router.route("/reportChat").post(protect, reportChat);
 
 
 module.exports = router;
